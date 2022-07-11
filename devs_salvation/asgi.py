6ffab5dd89b844ec0f94a1866a3e89d686edb152
Victor <vlcampos921@gmail.com>
2022-07-11 16:10:52 -0300

    feat: Create project initial structure

---
+++ devs_salvation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for devs_salvation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devs_salvation.settings')
+
+application = get_asgi_application()
