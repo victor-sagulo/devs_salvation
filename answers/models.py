63ff067c1020da5a1afcbf79adb8a551ff7641d5
Victor <vlcampos921@gmail.com>
2022-07-11 17:54:00 -0300

    feat: Create answers model

--- answers/models.py
+++ answers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Answer(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    usefull_post = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    deslikes = models.IntegerField(default=0)
+    user = models.ForeignKey(
+        "accounts.User", on_delete=models.CASCADE, related_name='answers')
+    post = models.ForeignKey(
+        "posts.Post", on_delete=models.CASCADE, related_name='answers')
