1e317bf850d0eebad59e134cb787f3cbd8f79abf
Gabriel Castedo <gabriel.castedo1@gmail.com>
2022-07-12 19:42:11 -0400

    feat: url accounts

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("accounts/login/", views.LoginView.as_view()),
+    path("accounts/", views.ListCreateUserView.as_view()),
+    path("accounts/<user_id>/", views.RetrieveUpdateDestroyView.as_view()),
+]
