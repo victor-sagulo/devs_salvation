0c878b941ddc6f53128c9cb4e0cba7feec309219
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-07-12 15:04:56 -0300

    feat: List and Create User GenericView

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from rest_framework import serializers
+from rest_framework import serializers, generics
 from rest_framework.views import APIView, Response, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import authenticate
-from accounts.serializers import LoginSerializer
-# Create your views here.
+from accounts.serializers import LoginSerializer, AccountsSerializer
+from accounts.models import User
 
+
+class ListCreateUserView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = AccountsSerializer
+    
 
 class LoginView(APIView):
     def post(self, request):
